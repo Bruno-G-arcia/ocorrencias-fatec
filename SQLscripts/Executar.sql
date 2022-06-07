9bb6295330d49eed3dbd44aa87274210cf258b8a
Bruno-G-arcia <86625680+Bruno-G-arcia@users.noreply.github.com>
2022-06-07 17:54:14 -0300

    Implementacoes de controle de funcionario

--- SQLscripts/Executar.sql
+++ SQLscripts/Executar.sql
@@ -1,9 +1,3 @@
 USE OCORRENCIAS;
 
-SELECT Usuario.IdUsuario, Usuario.Email,
-Usuario.Tipo, Pessoa.CPF, Pessoa.IdPessoa,
-Pessoa.Nome, Pessoa.Telefone,
-Cliente.IdCliente
-FROM Cliente, Pessoa, Usuario
-WHERE Pessoa.IdPessoa = Cliente.IdPessoa
-AND Usuario.IdUsuario = Cliente.IdUsuario;
+SELECT * FROM OrdemServico;
